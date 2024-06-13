fda2856f8f44e1ad27c2b21961838f4c27c2ef64
RyanBarszcz <113656726+RyanBarszcz@users.noreply.github.com>
2024-06-12 20:29:46 -0400

    Implement transaction table and transaction history tab

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import { sidebarLinks } from '@/constants'
 import { cn } from '@/lib/utils'
 import { usePathname } from 'next/navigation'
 import Footer from './Footer';
+import PlaidLink from './PlaidLink';
 
 const Sidebar = ({user}: SiderbarProps) => {
 
@@ -54,7 +55,9 @@ const Sidebar = ({user}: SiderbarProps) => {
                 </Link>
                 )
             })}
-            USER
+            <PlaidLink 
+            user={user}
+            />
         </nav>
         <Footer user ={user}/>
     </section>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -10,6 +10,7 @@ import { plaidClient } from '@/lib/plaid';
 import { revalidatePath } from "next/cache";
 import { addFundingSource, createDwollaCustomer } from "./dwolla.actions";
 import { parse } from "path";
+import { create } from "domain";
 
 const {
     APPWRITE_DATABASE_ID: DATABASE_ID,
@@ -17,9 +18,10 @@ const {
     APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID,
   } = process.env;
 
-  export const getUserInfo = async ({userId}: getUserInfoProps) => {
+  export const getUserInfo = async ({ userId }: getUserInfoProps) => {
     try {
         const {database} = await createAdminClient();
+        
         const user = await database.listDocuments(
             DATABASE_ID!,
             USER_COLLECTION_ID!,
@@ -43,7 +45,7 @@ export const signIn = async ({email, password}: signInProps) => {
             secure: true,
         });
 
-        const user = await getUserInfo({userId: session.userId})
+        const user = await getUserInfo({ userId: session.userId })
 
         return parseStringify(user);
     } catch (error) {
@@ -240,5 +242,37 @@ export const exchangePublicToken = async ({
         });
     } catch (error) {
         console.error('An error occurred while creating exchanging token:', error);
+    }
+}
+
+export const getBanks = async ({ userId }: getBanksProps) => {
+    try {
+        const { database } = await createAdminClient();
+
+        const banks = await database.listDocuments(
+            DATABASE_ID!,
+            BANK_COLLECTION_ID!,
+            [Query.equal('userId', [userId])]
+        )
+
+        return parseStringify(banks.documents);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getBank = async ({ documentId }: getBankProps) => {
+    try {
+        const { database } = await createAdminClient();
+
+        const bank = await database.listDocuments(
+            DATABASE_ID!,
+            BANK_COLLECTION_ID!,
+            [Query.equal('$id', [documentId])]
+        )
+
+        return parseStringify(bank.documents[0]);
+    } catch (error) {
+        console.log(error)
     }
 }
